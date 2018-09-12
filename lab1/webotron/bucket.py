3df2390d1aeef9104337f2c05f108e41d2cb4397
sjaipuri87 <sjaipuri87@gmail.com>
2018-09-11 18:35:40 -0700

    print website host when syncing

--- lab1/webotron/bucket.py
+++ lab1/webotron/bucket.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import mimetypes
 import os
 from botocore.exceptions import ClientError
+import util
 
 class BucketManager:
     """Manage an S3 Bucket."""
@@ -9,6 +10,16 @@ class BucketManager:
         """Create a BucketManager object."""
         self.session = session
         self.s3 = self.session.resource('s3')
+
+    def get_region_name(self, bucket):
+        """ Get the bucket's region name."""
+        client = self.s3.meta.client
+        bucket_location = client.get_bucket_location(Bucket=bucket.name)
+        return bucket_location["LocationConstraint"] or 'us-east-1'
+
+    def get_bucket_url(self,bucket):
+        """Get website URL"""
+        return "http://{}.{}".format(bucket.name, util.get_endpoint(self.get_region_name(bucket)).host)
     def all_buckets(self):
         """Get an iterator for all buckets."""
         return self.s3.buckets.all()
--- lab1/webotron/webotron.py
+++ lab1/webotron/webotron.py
@@ -9,6 +9,7 @@ import click
 import mimetypes
 import platform
 import boto3
+import util
 
 from bucket import BucketManager
 
@@ -58,6 +59,7 @@ def setup_bucket(bucket):
 def sync(pathname, bucket):
     """Sync contents of PATHNAME to BUCKET"""
     bucket_manager.sync(pathname, bucket)
+    print(bucket_manager.get_bucket_url(bucket_manager.s3.Bucket(bucket)))
     print("Website Sync is done")
 
 if __name__ == '__main__':
